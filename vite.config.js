9e4f200a56e5e0ebc44eac3539dcdd13db242f37
zaidanpoin <zaidanpoin123@gmail.com>
2025-01-30 18:22:40 +0700

    feat : "menambahkan sebagian landing page"

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         laravel({
+
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
