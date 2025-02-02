5e2591b40fc730f92634e12944a3c3f0e8b5d2d2
zaidanpoin <zaidanpoin123@gmail.com>
2025-02-02 15:31:26 +0700

    fix crud in users

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -30,7 +30,7 @@ class UserResource extends Resource
                     ->email()
                     ->required()
                     ->maxLength(255),
-                Forms\Components\DateTimePicker::make('email_verified_at'),
+
                 Forms\Components\TextInput::make('password')
                     ->password()
                     ->required()
