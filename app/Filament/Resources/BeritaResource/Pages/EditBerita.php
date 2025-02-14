f9dbc1ef32898e0ee0caff9cd3e1e7cc1d48d2a0
ryanadri2 <ryanadri2233@gmail.com>
2025-02-14 17:51:46 +0700

    feat:"fix redirect"

--- app/Filament/Resources/BeritaResource/Pages/EditBerita.php
+++ app/Filament/Resources/BeritaResource/Pages/EditBerita.php
@@ -16,4 +16,9 @@ class EditBerita extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/WisataResource/Pages/CreateWisata.php
+++ app/Filament/Resources/WisataResource/Pages/CreateWisata.php
@@ -9,4 +9,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateWisata extends CreateRecord
 {
     protected static string $resource = WisataResource::class;
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
